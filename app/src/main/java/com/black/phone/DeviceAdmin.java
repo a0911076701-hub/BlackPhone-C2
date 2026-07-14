aadd7dcd3b8a805694d56ee5332c243de40275c6
a0911076701-hub <a0911076701@gmail.com>
2026-07-14 23:04:19 +0200

    Add DeviceAdmin.java

--- app/src/main/java/com/black/phone/DeviceAdmin.java
+++ app/src/main/java/com/black/phone/DeviceAdmin.java
@@ -3,15 +3,14 @@ package com.black.phone;
 import android.app.admin.DeviceAdminReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 public class DeviceAdmin extends DeviceAdminReceiver {
     @Override
     public void onEnabled(Context context, Intent intent) {
-        Toast.makeText(context, "مدير الجهاز مفعل", Toast.LENGTH_SHORT).show();
+        super.onEnabled(context, intent);
     }
     @Override
     public void onDisabled(Context context, Intent intent) {
-        Toast.makeText(context, "لا يمكن تعطيل المدير", Toast.LENGTH_SHORT).show();
+        super.onDisabled(context, intent);
     }
 }
