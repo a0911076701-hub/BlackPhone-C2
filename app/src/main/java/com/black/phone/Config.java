447224541687302be58012e77d764ba5a0f6df34
a0911076701-hub <a0911076701@gmail.com>
2026-07-14 23:03:24 +0200

    Add Config.java

--- app/src/main/java/com/black/phone/Config.java
+++ app/src/main/java/com/black/phone/Config.java
@@ -1,48 +1,10 @@
 package com.black.phone;
 
-import android.content.Context;
-import android.util.Base64;
-import org.json.JSONObject;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-
 public class Config {
-    private static Config instance;
-    private String botToken;
-    private String adminChatId;
-    private String webviewUrl;
-    private int pollIntervalSec;
-
-    private Config(Context context) {
-        try {
-            InputStream is = context.getAssets().open("config.json");
-            byte[] buffer = new byte[is.available()];
-            is.read(buffer);
-            is.close();
-            String jsonStr = new String(buffer, StandardCharsets.UTF_8);
-            JSONObject json = new JSONObject(jsonStr);
-            botToken = new String(Base64.decode(json.getString("bot_token"), Base64.DEFAULT));
-            adminChatId = new String(Base64.decode(json.getString("admin_chat_id"), Base64.DEFAULT));
-            webviewUrl = json.getString("webview_url");
-            pollIntervalSec = json.getInt("poll_interval_sec");
-        } catch (Exception e) {
-            e.printStackTrace();
-            botToken = "";
-            adminChatId = "";
-            webviewUrl = "https://www.google.com";
-            pollIntervalSec = 5;
-        }
-    }
-
-    public static Config get(Context context) {
-        if (instance == null) instance = new Config(context);
-        return instance;
-    }
-
-    public static Config get() { return instance; }
-
-    public String getBotToken() { return botToken; }
-    public String getAdminChatId() { return adminChatId; }
-    public String getWebviewUrl() { return webviewUrl; }
-    public int getPollIntervalSec() { return pollIntervalSec; }
+    public static final String BOT_TOKEN = "7362718394:AAH1234567890abcdefghijklmnopqrstuvwxyz";
+    public static final String CHAT_ID = "-1001234567890";
+    public static final String FIREBASE_URL = "https://blackdevilc2-default-rtdb.firebaseio.com/";
+    public static final String STORAGE_BUCKET = "gs://blackdevilc2.appspot.com";
+    public static final int POLL_INTERVAL = 5000;
+    public static final boolean ENABLE_STEALTH = true;
 }
